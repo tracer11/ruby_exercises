5b9c480fb07b50d0b2a03a80b3eb3078364da6fe
tracer11 <karolgurba22@gmail.com>
2017-04-24 21:31:51 -0500

    added a class

---
+++ employee.rb
@@ -0,0 +1,34 @@
+class Employee
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
+  end
+  attr_reader :first_name, :last_name, :salary, :active
+  attr_writer :active 
+
+  def print_info
+    puts "#{first_name} #{last_name} makes #{salary} a year"
+  end
+
+  def give_annual_raise
+    @salary = @salary * 1.05
+  end
+end 
+
+employee_1 = Employee.new(
+  first_name: "Jim", 
+  last_name: "Jeffries",
+  salary: 70000,
+  active: true
+    )
+employee_2 = Employee.new(first_name: "Maria", last_name: "Bamford", salary: 80000, active: true)
+
+employee_1.print_info
+employee_2.print_info
+puts employee_2.salary
+puts employee_2.active
+puts employee_1.active 
+puts employee_1.active = false
+puts employee_1.active 
